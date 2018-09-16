767f108ebed470d74b5e1761482a01117f4a6b96
System Administrator <root@beants-MacBook-Air.local>
2018-09-17 01:25:47 +0530

    firstcommit

---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+@Component({
+    selector : 'alert-warming',
+    templateUrl : './warningalert.component.html' ,
+    styleUrls : ['./warningalert.component.html']
+})
+export class WarningAlert{
+
+}
